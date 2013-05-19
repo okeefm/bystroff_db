d7d97f10610731cd42594a747e936f166500d21e
Michael O'Keefe <okeefm@rpi.edu>
2013-05-18 22:12:36 -0400

    Removed useless echos from update_location

--- public_html/ajax/update_location.php
+++ public_html/ajax/update_location.php
@@ -1,6 +1,25 @@
 <?php
-	echo "ID: ";
-	echo $_POST["id"];
-	echo " VALUE: ";
-	echo $_POST["value"];
+	$db = parse_ini_file("../config/db.ini");
+	$mysqli = new mysqli($db["host"], $db["user"], $db["pass"], $db["name"], $db["port"]);
+	$query = "";
+	switch ($_POST["selectId"]) {
+		case "locations":
+			$query = "UPDATE locations SET value=? WHERE id=?;";
+			break;
+		case "sublocations":
+			$query = "UPDATE sublocations SET value=? WHERE id=?;";
+			break;
+		case "boxes":
+			$query = "UPDATE boxes SET value=? WHERE id=?;";
+			break;
+		default:
+			die("Not a selectId name");
+	}
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("sd", $_POST["value"], $_POST["id"]);
+	if ($stmt->execute()) {
+		echo "success";
+	} else {
+		echo "failure";
+	}
 ?>
