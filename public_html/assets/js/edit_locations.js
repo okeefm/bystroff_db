bb16dc4c815b758d6cc430e18f0f6a729ec38ee4
Michael O'Keefe <okeefm@rpi.edu>
2013-05-18 22:13:00 -0400

    Added update and delete code to edit_locations.js

--- public_html/assets/js/edit_locations.js
+++ public_html/assets/js/edit_locations.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 		init: function () {
 			$("input.edit").click(function(e) {
 				e.preventDefault();
-				console.log(this);
+				
+				var selectId = $(this).prev().find("select").attr("id");
 				var option = $(this).prev().find("select option:selected");
 				if (option.val() == "") {
 					alert("Please select an object to edit");
@@ -14,7 +15,7 @@ $(document).ready(function() {
 				$("#editDialog").modal({
 					autoResize:true,
 					overlayClose:true,
-					onOpen: edit.open(option)
+					onOpen: edit.open(option, selectId)
 				});
 				
 				$("#editConfirm").click(function(e) {
@@ -22,12 +23,16 @@ $(document).ready(function() {
 					if (confirm("Are you sure you want to edit this?")) {
 						console.log("foo");
 						$.post("ajax/update_location.php",
-						{ id: $("#editInput").data("id"), value: $("#editInput").val()},
+						{ id: $("#editInput").data("id"), value: $("#editInput").val(), selectId: $("#editInput").data("selectId")},
 						function(data) {
 							console.log(data);
-							alert("Item successfully edited.");
-							$("#editCancel").click();
-							location.reload();
+							if (data == "success") {
+								alert("Item successfully edited.");
+								$("#editCancel").click();
+								location.reload();
+							} else {
+								alert("Update failed!");
+							}
 						});
 					}
 				});
@@ -36,14 +41,41 @@ $(document).ready(function() {
 		},
 		
 		//this gets run when the modal is opened, and adds the option text to the text box, and option value as data
-		open: function(option) {
+		open: function(option, selectId) {
 			$("#editInput").val(option.text());
 			$("#editInput").data("id", option.val());
+			$("#editInput").data("selectId", selectId);
 		}
 
 	}
 	
+	var del = {
+		init: function() {
+			$("input.delete").click(function(e) {
+				e.preventDefault();
+				
+				var si = $(this).prev().prev().find("select").attr("id");
+				var option = $(this).prev().prev().find("select option:selected").val();
+				
+				if (confirm("Are you sure you want to delete this?")) {
+					$.post("ajax/delete_location.php",
+						{ id: option, selectId: si},
+						function(data) {
+							if (data == "success") {
+								alert("Item successfully deleted.");
+								location.reload();
+							} else {
+								alert("Delete failed!");
+							}
+						});
+				}
+			});
+
+		}
+	}
+	
 	edit.init();
+	del.init();
 	
 	$("select").change(function(e) {
 		var nextId = null;
