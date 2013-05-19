34a7136734d0db37959fc30fce6a02cd068ffa54
Michael O'Keefe <okeefm@rpi.edu>
2013-05-18 22:13:23 -0400

    Created delete_location.php

---
+++ public_html/ajax/delete_location.php
@@ -0,0 +1,25 @@
+<?php
+	$db = parse_ini_file("../config/db.ini");
+	$mysqli = new mysqli($db["host"], $db["user"], $db["pass"], $db["name"], $db["port"]);
+	$query = "";
+	switch ($_POST["selectId"]) {
+		case "locations":
+			$query = "DELETE FROM locations WHERE id=?;";
+			break;
+		case "sublocations":
+			$query = "DELETE FROM sublocations WHERE id=?;";
+			break;
+		case "boxes":
+			$query = "DELETE FROM boxes WHERE id=?;";
+			break;
+		default:
+			die("Not a selectId name");
+	}
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("d", $_POST["id"]);
+	if ($stmt->execute()) {
+		echo "success";
+	} else {
+		echo "failure";
+	}
+?>
