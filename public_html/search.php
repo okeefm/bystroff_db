775eb38dfa6d202fb4dbda69c7b8aa7825d81f35
Michael O'Keefe <okeefm@rpi.edu>
2013-05-31 09:26:47 -0400

    named search_db form

--- public_html/search.php
+++ public_html/search.php
@@ -1,7 +1,8 @@
-<form class="form-horizontal" target="search_db" method="POST">
+<form class="form-horizontal" target="search_db" id="search_db" method="POST">
 
 	<?php include "form_internals.php"; ?>
 	
 	<input class="btn" type="submit" name="submitSearch" value="Search" /> &nbsp;
 	<input class="btn" type='submit' name="reset" id="reset" value="Reset" />
+	
 </form>
