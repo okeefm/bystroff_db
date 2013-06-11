4af74a3cbc76c9645b9089e72131cbe5cf5b6235
Michael O'Keefe <okeefm@rpi.edu>
2013-06-11 12:43:45 -0400

    added edit/delete button handlers to search_db.js

--- public_html/assets/js/search_db.js
+++ public_html/assets/js/search_db.js
@@ -1,3 +1,49 @@
 $(document).ready(function() {
 	$("#results").dataTable();
+	
+	var edit = {
+		init: function () {
+			$("a.edit").click(function(e) {
+				e.preventDefault();
+				
+				$("#editDialog").modal({
+					autoResize:true,
+					overlayClose:true
+					});
+				
+				$("#editConfirm").data("type", "edit");
+				$("#editInput").val(option.text());
+				$("#editInput").data("id", option.val());
+				$("#editInput").data("selectId", selectId);
+				$("#editTitle").text("Edit");
+			});
+			
+		}
+
+	}
+	
+	var del = {
+		init: function() {
+			$("a.delete").click(function(e) {
+				e.preventDefault();
+				
+				if (confirm("Are you sure you want to delete this sample?")) {
+					$.post("ajax/delete_location.php",
+						{ id: option, selectId: si},
+						function(data) {
+							if (data == "success") {
+								alert("Item successfully deleted.");
+								location.reload();
+							} else {
+								alert("Delete failed!");
+							}
+						});
+				}
+			});
+
+		}
+	}
+	
+	edit.init();
+	del.init();
 });
