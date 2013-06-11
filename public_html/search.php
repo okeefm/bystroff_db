10b823ae506cf56e8d76162cea25a53f6b171a06
Michael O'Keefe <okeefm@rpi.edu>
2013-06-11 12:22:05 -0400

    Added user tip to search.php

--- public_html/search.php
+++ public_html/search.php
@@ -4,5 +4,9 @@
 	
 	<input class="btn" type="submit" name="submitSearch" value="Search" /> &nbsp;
 	<input class="btn" type='submit' name="reset" id="reset" value="Reset" />
-	
+		
+	<br /> <br />
+	<div>
+		Tip: to search for a partial sequence, use the wildcard operator (" * ") at the ends of the search string (e.g. " *CAGCGG* "). To search for a partial sequence at the start or end of a sequence, use a wildcard operator only at one end of the search string.
+	</div>
 </form>
