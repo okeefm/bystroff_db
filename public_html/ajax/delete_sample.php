8cc0603ea2164e03ee1630d60a59140b6afaca8e
Michael O'Keefe <okeefm@rpi.edu>
2013-06-11 12:54:37 -0400

    Made delete_sample.php functional

--- public_html/ajax/delete_sample.php
+++ public_html/ajax/delete_sample.php
@@ -1,3 +1,13 @@
 <?php
-
+	$db = parse_ini_file("../config/db.ini");
+	$mysqli = new mysqli($db["host"], $db["user"], $db["pass"], $db["name"], $db["port"]);
+	$stmt = $mysqli->prepare("DELETE FROM samples WHERE id = ?");
+	$stmt->bind_param("d", $_POST["id"]);
+	if ($stmt->execute()) {
+		echo "success";
+	} else {
+		echo "failure";
+	}
+	$mysqli->close();
+	
 ?>
