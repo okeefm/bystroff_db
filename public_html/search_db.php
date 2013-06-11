99688dba3958445a4a7e5cf714f666332f4ee7d8
Michael O'Keefe <okeefm@rpi.edu>
2013-06-11 12:23:17 -0400

    Updated search_db extensively

--- public_html/search_db.php
+++ public_html/search_db.php
@@ -1,11 +1,11 @@
 <?php
-	include('assets/php/bootstrap.php');
+	include "header.php";
+	include "assets/php/bootstrap.php";
+	
 	// Set the default timezone to use. Available as of PHP 5.1
 	date_default_timezone_set('UTC');
 	
-	var_dump($_POST);
-	
-	echo("<br /> <br />");
+	//var_dump($_POST);
 	
 	$instance_KoSolr = KoSolr::getInstance();
 	$KoSolr_Server_Instance = $instance_KoSolr->getServer();
@@ -39,8 +39,131 @@
 		echo $date;
 		$search_request->equals('date', $date);
 	}
+	if (isset($_POST['sequence']) && (strlen($_POST['sequence']) > 0)) {
+		$search_request->equals('sequence', $_POST['sequence']);
+	}
 	
 	$response = $KoSolr_Server_Instance->execute($search_request);
 	
-	var_dump($response);
+	echo "<table class='table table-bordered' id='results'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Sample Name</th>
+                    <th>Type</th>
+                    <th>Room</th>
+                    <th>Location</th>
+                    <th>Box</th>
+                    <th>Date</th>
+                    <th>Owner</th>
+                    <th>Concentration</th>
+                    <th>Volume</th>
+                    <th>GI Number</th>
+                    <th>Comments</th>
+                    <th>Sequence</th>
+                </tr>
+            </thead>";
+			
+	$i = 0;
+	foreach($response->response['docs'] as $result) {
+		echo "<tr class='tableRow'> \n";
+		echo "<td>".$i."</td>";
+		
+		if (isset($result["Name_of_Sample"])) {
+			echo "<td>".$result["Name_of_Sample"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["type"])) {
+			echo "<td>".$result["type"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["location"])) {
+			echo "<td>".$result["location"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["sublocation"])) {
+			echo "<td>".$result["sublocation"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["box"])) {
+			echo "<td>".$result["box"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["date"])) {
+			echo "<td>".$result["date"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["owner"])) {
+			echo "<td>".$result["owner"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["concentration"])) {
+			echo "<td>".$result["concentration"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["volume"])) {
+			echo "<td>".$result["volume"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["gi_number"])) {
+			echo "<td>".$result["gi_number"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["comments"])) {
+			echo "<td>".$result["comments"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		if (isset($result["sequence"])) {
+			echo "<td>".$result["sequence"]."</td>\n";
+		} else {
+			echo "<td> </td>\n";
+		}
+		
+		echo "</tr>\n";
+	}
+	unset($result);
+	
+	echo "<tfoot>
+                <tr>
+                    <th>#</th>
+                    <th>Sample Name</th>
+                    <th>Type</th>
+                    <th>Room</th>
+                    <th>Location</th>
+                    <th>Box</th>
+                    <th>Date</th>
+                    <th>Owner</th>
+                    <th>Concentration</th>
+                    <th>Volume</th>
+                    <th>GI Number</th>
+                    <th>Comments</th>
+                    <th>Sequence</th>
+                </tr>
+            </tfoot>";
+			
+	include "footer.php";
 ?>
+
+<script src="assets/js/search_db.js"></script>
