a3cb01493d18fea301a25d45f74631916412ae24
Michael O'Keefe <okeefm@rpi.edu>
2013-06-12 00:02:09 -0400

    Put in additional fields for new samples, on top of form internals

---
+++ public_html/new_sample_additions.php
@@ -0,0 +1,29 @@
+
+<div class="control-group">
+	<label class="control-label" for="comments">Comments</label>
+	<div class="controls">
+		<textarea name="comments" id="comments"></textarea>
+		<span class="help-block">Comments about this sample.</span>
+	</div>
+</div>
+
+<div class="control-group">
+	<label class="control-label" for="concentration">Concentration</label>
+	<div class="controls">
+		<input type="text" name="concentration" id="concentration" />
+	</div>
+</div>
+
+<div class="control-group">
+	<label class="control-label" for="amount">Amount</label>
+	<div class="controls">
+		<input type="text" name="amount" id="amount" />
+	</div>
+</div>
+
+<div class="control-group">
+	<label class="control-label" for="purity">Purity</label>
+	<div class="controls">
+		<input type="text" name="purity" id="purity" />
+	</div>
+</div>
