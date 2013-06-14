23648065bc263b0acb8253621f04f1f3c938489d
Michael O'Keefe <okeefm@rpi.edu>
2013-06-14 00:18:40 -0400

    Created edit_sample.php

---
+++ public_html/edit_sample.php
@@ -0,0 +1,36 @@
+<?php include "header.php"; ?>
+
+<?php
+	$mysqli = new mysqli($db["host"], $db["user"], $db["pass"], $db["name"], $db["port"]);
+	$stmt = $mysqli->prepare("SELECT id, Name_of_Sample, Location, Sublocation, Box, Owner, Date, Type, Gi_number, Sequence, Comments, Concentration, Amount, Purity FROM samples WHERE id = ?;");
+	$stmt->bind_param("d", $_REQUEST['id']);
+	if($stmt->execute()) {
+		$res = $stmt->get_result();
+		$edit_row = $res->fetch_assoc();
+	} else {
+		
+	}
+	
+	var_dump($edit_row);
+	
+	$edit = True;
+?>
+
+<div class="container">
+
+	<h2>Edit Sample</h2>
+	<div class="well">
+		<form class="form-horizontal" action="edit_sample.php" method="POST">
+
+			<?php 
+			include "form_internals.php";
+			
+			include "new_sample_additions.php"; ?>
+			<input class="btn" type="submit" name="submitAdd" value="Submit" /> &nbsp;
+			<input class="btn" type='submit' name="reset" id="reset" value="Reset" />
+		</form>
+	</div>
+
+</div>
+
+<?php include "footer.php"; ?>
